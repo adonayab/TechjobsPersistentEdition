7993f575660d49ad79d2e59ed0af9f4f11659978
ado@ado <adonbayab15@hotmail.com>
2023-03-30 22:44:20 -0500

    pushing techjobs persistent

---
+++ src/main/java/org/launchcode/techjobs/persistent/models/Job.java
@@ -0,0 +1,40 @@
+package org.launchcode.techjobs.persistent.models;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+public class Job extends AbstractEntity {
+
+    @ManyToOne
+    private Employer employer;
+
+    @ManyToMany
+    private List<Skill> skills = new ArrayList<>();
+
+    public Job() {
+    }
+
+    public Job(Employer anEmployer, List<Skill> skills) {
+        super();
+        this.employer = anEmployer;
+        this.skills = skills;
+    }
+
+    public Employer getEmployer() {
+        return employer;
+    }
+
+    public void setEmployer(Employer employer) {
+        this.employer = employer;
+    }
+
+    public List<Skill> getSkills() {
+        return skills;
+    }
+
+    public void setSkills(List<Skill> skills) {
+        this.skills = skills;
+    }
+}
